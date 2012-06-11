f53f80430c835ed445d4d6a77a6ed5f7661ea57e
Anton <AntonShevchuk@gmail.com>
2012-06-11 11:35:42 +0300

    Move helpers

--- application/layouts/helpers/Link.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return function ($src = null, $rel = 'stylesheet') {
-    if (null === $src) {
-        echo $this->headLinkFiles;
-    } else {
-        $this->headLinkFiles .= '<link type="text/css" href="' . $this->baseUrl($src) . '" rel="' . $rel .'"/>'."\n";
-    }
-};
--- application/layouts/helpers/style.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return function (\Bluz\View\View $view, $src = null, $rel = 'stylesheet') {
-    if (null === $src) {
-        echo $view->headLinkFiles;
-    } else {
-        $view->headLinkFiles .= '<link type="text/css" href="' . $view->baseUrl($src) . '" rel="' . $rel .'"/>'."\n";
-    }
-};
