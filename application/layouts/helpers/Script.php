4eddc97afb95318935f5e56515f17cd6eace3a93
Anton <AntonShevchuk@gmail.com>
2012-06-11 11:19:15 +0300

    08.06.2012 snapshot

--- application/layouts/helpers/Script.php
+++ application/layouts/helpers/Script.php
@@ -1,20 +1,20 @@
 <?php
 
-return function (\Bluz\View\View $view, $script = null) {
+return function ($script = null) {
     if (null === $script) {
-        echo $view->headScriptFiles;
-        if ($view->headScriptContent) {
+        echo $this->headScriptFiles;
+        if ($this->headScriptContent) {
             ?>
             <script type="text/javascript">
             <!--
-                <?php echo $view->headScriptContent;?>
+                <?php echo $this->headScriptContent;?>
             //-->
             </script>
             <?php
         }
     } elseif ('.js' == substr($script, -3, 3)) {
-        $view->headScriptFiles .= '<script type="text/javascript" src="' . $view->baseUrl($script) .'"></script>'."\n";
+        $this->headScriptFiles .= '<script type="text/javascript" src="' . $this->baseUrl($script) .'"></script>'."\n";
     } else {
-        $view->headScriptContent .= $script . ';';
+        $this->headScriptContent .= $script . ';';
     }
 };
