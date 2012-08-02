9987b6c7db78c210acec23d689988c07679fe330
Anton <AntonShevchuk@gmail.com>
2012-08-02 15:23:10 +0300

    Move Partial and PartialLoop to Helpers from View class

---
+++ application/modules/users/controllers/grid.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Grid of users
+ *
+ * @author   Anton Shevchuk
+ * @created  02.08.12 18:39
+ * @return closure
+ */
+namespace Application;
+return
+/**
+ * @return \closure
+ */
+function() {
+
+    // change layout
+    $this->useLayout('grid.phtml');
+};
