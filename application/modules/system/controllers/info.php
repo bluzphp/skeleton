034bce058401086627373bf2cc3685502bd7991c
Anton <AntonShevchuk@gmail.com>
2012-09-07 18:52:06 +0300

    More simple phpinfo call

--- application/modules/system/controllers/info.php
+++ application/modules/system/controllers/info.php
@@ -14,5 +14,6 @@ return
  * @return \closure
  */
 function() {
-
+    phpinfo();
+    return false;
 };
--- application/modules/test/controllers/db.php
+++ application/modules/test/controllers/db.php
@@ -8,11 +8,15 @@
 namespace Application;
 
 return
+/**
+ * @TODO: need more informative example
+ * @return \closure
+ */
 function() {
     /**
      * @var Bootstrap $this
      */
-    $res = $this->getDb()->fetchAll('SELECT * FROM test WHERE name LIKE "a%"');
+    $res = $this->getDb()->fetchAll('SELECT * FROM test WHERE name LIKE ?', ['al%']);
     var_dump($res);
     return false;
 };
