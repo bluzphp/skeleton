5f768b01254f2477abee880953f3bae147cdcb51
Anton <AntonShevchuk@gmail.com>
2012-10-29 16:45:24 +0200

    Added twitter widget for auth

---
+++ application/modules/twitter/widgets/auth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author   Anton Shevchuk
+ * @created  22.10.12 18:40
+ */
+namespace Application;
+return
+/**
+ * @return \closure
+ */
+function () {
+    /**
+     * @var \Bluz\Application $this
+     */
+    ?>
+    <a href="<?=$this->getRouter()->url('twitter', 'auth')?>" class="btn btn-primary">Twitter</a>
+    <?php
+};
+?>
