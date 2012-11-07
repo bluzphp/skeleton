4f0beedae0b5cca60ceda2cdf97393654ff059a3
Pavel Machekhin <pavel.machekhin@gmail.com>
2012-11-07 13:41:59 +0200

    Repaired path in tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ define('PATH_PUBLIC', PATH_ROOT . '/public');
 define('PATH_THEME', PATH_ROOT . '/themes');
 
 // init loader
-require_once PATH_LIBRARY . '/Bluz/_loader.php';
+require_once PATH_LIBRARY . '/autoload.php';
 require_once 'application/BootstrapTest.php';
 require_once 'application/TestCase.php';
 require_once 'application/TestListener.php';
