b53f5e90bf59a15a419bac76f77248dab70d5ef3
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2012-12-29 11:33:04 +0200

    Rename default error controller from 'error' to 'index'

--- tests/modules/error/controllers/ErrorTest.php
+++ tests/modules/error/controllers/ErrorTest.php
@@ -16,7 +16,7 @@ class ErrorTest extends TestCase
      */
     public function testError403()
     {
-        $this->app->dispatch('error', 'error', ['message'=>'Error', 'code'=>403]);
+        $this->app->dispatch('error', 'index', ['message'=>'Error', 'code'=>403]);
         $this->assertEquals($this->app->getLayout()->title(), '403 Forbidden');
     }
 
@@ -27,7 +27,7 @@ class ErrorTest extends TestCase
      */
     public function testError404()
     {
-        $this->app->dispatch('error', 'error', ['message'=>'Error', 'code'=>404]);
+        $this->app->dispatch('error', 'index', ['message'=>'Error', 'code'=>404]);
         $this->assertEquals($this->app->getLayout()->title(), '404 Not Found');
     }
 }
