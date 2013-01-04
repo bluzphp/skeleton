7f98cf3d9b6903db91ec9dc61f7e113c9d40125e
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-01-04 13:07:13 +0200

    added definitions over APC

--- public/_loader.php
+++ public/_loader.php
@@ -12,13 +12,34 @@ if (version_compare(phpversion(), '5.4.3', '<') ) {
     exit();
 }
 
+// Check APC and use it for definitions
+if (function_exists('apc_load_constants')) {
+    function define_array($key, $arr, $case_sensitive = true)
+    {
+        if (!apc_load_constants($key, $case_sensitive)) {
+            apc_define_constants($key, $arr, $case_sensitive);
+        }
+    }
+} else {
+    function define_array($key, $arr, $case_sensitive = true)
+    {
+        foreach ($arr as $name => $value)
+            define($name, $value, $case_sensitive);
+    }
+}
+
+
 // Paths
-define('PATH_ROOT', realpath(dirname(__FILE__) . '/../'));
-define('PATH_APPLICATION', PATH_ROOT . '/application');
-define('PATH_DATA', PATH_ROOT . '/data');
-define('PATH_VENDOR', PATH_ROOT . '/vendor');
-define('PATH_BLUZ', PATH_VENDOR . '/bluzphp/framework/src/Bluz');
-define('PATH_PUBLIC', PATH_ROOT . '/public');
+$root = realpath(dirname(__FILE__) . '/../');
+
+define_array('PATH', array(
+    'PATH_ROOT' => $root,
+    'PATH_APPLICATION' => $root . '/application',
+    'PATH_DATA' => $root . '/data',
+    'PATH_VENDOR' => $root . '/vendor',
+    'PATH_BLUZ' => $root . '/bluzphp/framework/src/Bluz',
+    'PATH_PUBLIC' => $root . '/public'
+));
 
 // Shutdown function for handle critical and other errors
 register_shutdown_function('errorHandler');
