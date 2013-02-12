23319da0ae34164c3ea102843da9871c0a1661c6
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-02-12 18:37:31 +0200

    Updated copyright year

--- application/models/Application/Pages/Crud.php
+++ application/models/Application/Pages/Crud.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2012 by Bluz PHP Team
+ * Copyright (c) 2013 by Bluz PHP Team
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -50,7 +50,7 @@ class Crud extends \Bluz\Crud\Crud
         $alias = $this->getData('alias');
         if (empty($alias)) {
             $this->addError('alias', 'Alias can\'t be empty');
-        } elseif (!preg_match('/^[a-zA-Z.-]+$/i', $alias)) {
+        } elseif (!preg_match('/^[a-zA-Z0-9_\.\-]+$/i', $alias)) {
             $this->addError('alias', 'Alias should contains only Latin characters, dots and dashes');
         } elseif ($row = $this->getTable()->findRowWhere(['alias' => $alias])) {
             if ($row->id != $this->getData('id')){
@@ -60,7 +60,7 @@ class Crud extends \Bluz\Crud\Crud
 
         // content validator
         $content = $this->getData('content');
-        if (empty($content) or trim(strip_tags($content)) == '') {
+        if (empty($content) or trim(strip_tags($content, '<img>')) == '') {
             $this->addError('content', 'Content can\'t be empty');
         }
 
