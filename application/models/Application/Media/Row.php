6c6faad688688d42558978c66a85e9b68970ee29
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-02-12 18:38:00 +0200

    Added Media module

---
+++ application/models/Application/Media/Row.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Copyright (c) 2013 by Bluz PHP Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @namespace
+ */
+namespace Application\Media;
+
+/**
+ * Options Row
+ *
+ * @category Application
+ * @package  Options
+ */
+class Row extends \Bluz\Db\Row
+{
+    /**
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * @var integer
+     */
+    public $userId;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var string
+     */
+    public $module = 'users';
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $file;
+
+    /**
+     * @var string
+     */
+    public $preview;
+
+    /**
+     * @var string
+     */
+    public $created;
+
+    /**
+     * @var string
+     */
+    public $updated;
+
+    /**
+     * __insert
+     *
+     * @return void
+     */
+    public function preInsert()
+    {
+        $this->created = gmdate('Y-m-d H:i:s');
+        if ($user = $this->getApplication()->getAuth()->getIdentity()) {
+            $this->userId = $user->id;
+        } else {
+            $this->userId = \Application\Users\Row::SYSTEM_USER;
+        }
+
+    }
+
+    /**
+     * __update
+     *
+     * @return void
+     */
+    public function preUpdate()
+    {
+        $this->updated = gmdate('Y-m-d H:i:s');
+
+
+    }
+
+    /**
+     * postDelete
+     *
+     * @return void
+     */
+    public function postDelete()
+    {
+        @unlink(PATH_PUBLIC .'/'. $this->file);
+        @unlink(PATH_PUBLIC .'/'. $this->preview);
+    }
+}
---
+++ application/modules/media/controllers/list.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * List of user images in JSON
+ *
+ * @category Application
+ *
+ * @author   dark
+ * @created  12.02.13 14:18
+ */
+namespace Application;
+
+return
+/**
+ * @return \closure
+ */
+function() {
+    /**
+     * @var \Bluz\Application $this
+     * @var Media\Table $mediaTable
+     */
+    $this->useJson();
+
+    $user = $this->getAuth()->getIdentity();
+
+    $mediaTable = Media\Table::getInstance();
+
+    $images = $mediaTable->findWhere('type LIKE (?) AND userId = ?', ['image/%', $user->id]);
+
+    $result = array();
+    foreach ($images as $image) {
+        $result[] = [
+            "title" => $image->title,
+            "image" => $image->file,
+            "thumb" => $image->file,
+            "folder" => $image->module,
+        ];
+    }
+    return $result;
+};
