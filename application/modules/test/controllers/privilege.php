69691f55208d299fccbe4564570d651ba36022c6
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-05-22 10:33:02 +0300

    Updated error controller

---
+++ application/modules/test/controllers/privilege.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ *
+ * @category Application
+ *
+ * @author   dark
+ * @created  21.05.13 16:55
+ */
+namespace Application;
+
+return
+/**
+ * @privilege Denied
+ * @return \closure
+ */
+function() use ($view) {
+    /**
+     * @var \Bluz\Application $this
+     * @var \Bluz\View\View $view
+     */
+          
+};
