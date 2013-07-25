0d6aadc9955c5ce40a5b48ebcdf3cb82a94fdc72
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-07-25 15:20:19 +0300

    Fixed code style of application

--- application/modules/facebook/widgets/auth.php
+++ application/modules/facebook/widgets/auth.php
@@ -4,19 +4,20 @@
  * @created  22.10.12 18:40
  */
 namespace Application;
+
 return
+/**
+ * @return \closure
+ */
+function () {
     /**
-     * @return \closure
+     * @var \Bluz\Application $this
+     * @var \Bluz\View\View $view
      */
-    function () {
-        /**
-         * @var \Bluz\Application $this
-         * @var \Bluz\View\View $view
-         */
-        ?>
+    ?>
     <a href="<?=$this->getRouter()->url('facebook', 'auth')?>">
-        <img src="<?= $this->getView()->baseUrl('/img/social-icons/facebook.png') ?>" alt='Use your facebook account!' />
+        <img src="<?= $this->getView()->baseUrl('/img/social-icons/facebook.png') ?>"
+             alt="<?= _("Use your facebook account!")?>" />
     </a>
     <?php
-    };
-?>
+};
