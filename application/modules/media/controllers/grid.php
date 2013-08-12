eeb307c4889ac353ff4456d44238916430561c75
Petr Marchenko <PetrMarchenko@yandex.ru>
2013-08-12 23:58:45 +0300

    add setcookie

--- application/modules/media/controllers/grid.php
+++ application/modules/media/controllers/grid.php
@@ -1,33 +1,49 @@
 <?php
+
+
 /**
  * Grid of Options
  */
 namespace Application;
 
 return
-/**
- * @privilege Management
- * @return \closure
- */
-function () use ($view) {
     /**
-     * @var \Bluz\Application $this
-     * @var \Bluz\View\View $view
+     * @privilege Management
+     * @return \closure
      */
-    $this->getLayout()->setTemplate('dashboard.phtml');
-    $this->getLayout()->breadCrumbs(
-        [
-            $view->ahref('Dashboard', ['dashboard', 'index']),
-            __('Media')
-        ]
-    );
-    $grid = new Media\Grid();
+    function () use ($view) {
+        /**
+         * @var \Bluz\Application $this
+         * @var \Bluz\View\View $view
+         */
+        $this->getLayout()->setTemplate('dashboard.phtml');
+        $this->getLayout()->breadCrumbs(
+            [
+                $view->ahref('Dashboard', ['dashboard', 'index']),
+                __('Media')
+            ]
+        );
+        $grid = new Media\Grid();
 
 
-    $countCol = $this->getRequest()->getParam('countCol', 4);
-    $lnCol = (integer)(12/$countCol);
 
-    $view->countCol = $countCol;
-    $view->col = $lnCol;
-    $view->grid = $grid;
-};
+        $countCol = $this->getRequest()->getParam('countCol');
+
+        if(isset($countCol)){
+            $countCol = $countCol;
+            setcookie("countCol", $countCol, time()+3600, '/');
+        }else{
+            if (isset($_COOKIE["countCol"])){
+                $countCol = $_COOKIE["countCol"];
+            }else{
+                $countCol = 4;
+            }
+        }
+
+        $lnCol = (integer)(12/$countCol);
+
+        $view->countCol = $countCol;
+        $view->col = $lnCol;
+        $view->grid = $grid;
+    };
+
