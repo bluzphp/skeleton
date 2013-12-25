48f8d32ccbc33c1fe66fa65b734de7ff3ec605fa
Nogin Viacheslav <nogin.viacheslav@nixsolutions.com>
2013-12-25 18:09:13 +0200

    true formating

--- application/layouts/helpers/TreeBuild.php
+++ application/layouts/helpers/TreeBuild.php
@@ -6,22 +6,23 @@ return
         function ($tree) {
             $str = '';
 
-            foreach ($tree as $node) {
+    foreach ($tree as $node) {
 
-                if (empty($node['children'])) {
-                    $str .= '<li class="mjs-nestedSortable-leaf" data-order="' . $node['order'] . '" id="list_' . $node['id'] . '">'
-                            . $this->partial('category/navigation.phtml', ['node' => $node]) .
-                            '</li>';
-                } else {
-                    $str .= '<li class="mjs-nestedSortable-leaf" data-order="' . $node['order'] . '" id="list_' . $node['id'] . '">'
-                            . $this->partial('category/navigation.phtml', ['node' => $node]);
-                    $str .= '<ol>';
-                    $str .= $this->treeBuild($node['children']);
-                    $str .= '</ol>';
-                    $str .= '</li>';
-                }
-            }
+        if (empty($node['children'])) {
+            $str .= '<li class="mjs-nestedSortable-leaf" data-order="'
+                    . $node['order'] . '" id="list_' . $node['id'] . '">'
+            . $this->partial('category/navigation.phtml', ['node' => $node]) .
+            '</li>';
+        } else {
+            $str .= '<li class="mjs-nestedSortable-leaf" data-order="'
+                    . $node['order'] . '" id="list_' . $node['id'] . '">'
+            . $this->partial('category/navigation.phtml', ['node' => $node]);
+            $str .= '<ol>';
+            $str .= $this->treeBuild($node['children']);
+            $str .= '</ol>';
+            $str .= '</li>';
+        }
+    }
 
             return $str;
         };
-
