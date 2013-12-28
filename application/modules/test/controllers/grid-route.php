39eb21d1ebbec30d765bd62f22150f3f1c4591a0
Anton Shevchuk <AntonShevchuk@gmail.com>
2013-12-28 10:04:15 +0200

    Added examples of exceptions

---
+++ application/modules/test/controllers/grid-route.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Example of grid
+ *
+ * @author   Anton Shevchuk
+ * @created  27.08.12 10:08
+ */
+namespace Application;
+
+use Bluz;
+use Application\Test;
+
+return
+/**
+ * Example of Grid with custom route
+ *
+ * @route /example/{$alias}
+ * @param string $alias
+ * @return \closure
+ */
+function ($alias) use ($view, $module, $controller) {
+    /**
+     * @var \Application\Bootstrap $this
+     * @var \Bluz\View\View $view
+     */
+    $this->getLayout()->breadCrumbs(
+        [
+            $view->ahref('Test', ['test', 'index']),
+            'Grid with Select',
+        ]
+    );
+    $grid = new Test\SelectGrid();
+    $grid->setModule($module);
+    $grid->setController($controller);
+    $grid->setParams(['alias'=>$alias]);
+
+    $view->grid = $grid;
+
+    return 'grid-sql.phtml';
+};
