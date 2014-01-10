077624d4624190bfb2d91b3511a397a6afe9b126
Anton Shevchuk <AntonShevchuk@gmail.com>
2014-01-10 10:37:34 +0200

    Added Gd support for thumbnail generator

---
+++ application/library/Application/Image/Gd.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/skeleton
+ */
+
+/**
+ * @namespace
+ */
+namespace Application\Image;
+
+use Application\Exception;
+
+/**
+ * Wrapper over Gd for support some Imagick functions
+ *
+ * @category Application
+ * @package  Library
+ *
+ * @author   Anton Shevchuk
+ * @created  1/10/14 8:43 AM
+ */
+class Gd
+{
+    /**
+     * @var string
+     */
+    protected $file;
+
+    /**
+     * @var resource
+     */
+    protected $image;
+
+    /**
+     * @var integer
+     */
+    protected $width;
+
+    /**
+     * @var integer
+     */
+    protected $height;
+
+    /**
+     * Compression quality
+     * @var integer
+     */
+    protected $quality = 86;
+
+    /**
+     * Constructor of Gd
+     *
+     * @access  public
+     */
+    public function __construct($file)
+    {
+        if (!file_exists($file)) {
+            throw new Exception("Image `$file` file not found");
+        }
+        $this->file = $file;
+
+        // Retrieve image information
+        list($this->width, $this->height, $type) = getimagesize($file);
+
+        // Check support of file type
+        if (!(imagetypes() & $type)) {
+            throw new Exception("Server does not support `$type` image type");
+        }
+
+        // Using imagecreatefromstring will automatically detect the file type
+        $this->image = imagecreatefromstring(file_get_contents($file));
+
+        if (!$this->image) {
+            throw new Exception("Could not load image");
+        }
+    }
+
+    /**
+     * @param $quality
+     * @return bool
+     */
+    public function setImageCompressionQuality($quality)
+    {
+        $this->quality = $quality;
+        return true;
+    }
+
+    /**
+     * Create a crop thumbnail image from source image
+     * cropped by smaller
+     */
+    public function cropThumbnailImage($width, $height)
+    {
+        // Compare image size with required thumbnail size
+        if (($this->width < $width) &&
+            ($this->height < $height)) {
+            return true;
+        }
+
+        $widthScale = round($this->width / $width);
+        $heightScale = round($this->height / $height);
+
+        if ($heightScale < $widthScale) {
+            // Crop width
+            $cropWidth = $heightScale * $width;
+            $cropHeight = $this->height;
+            $srcX = round(($this->width - $cropWidth)/2);
+            $srcY = 0;
+        } else {
+            // Crop height
+            $cropWidth = $this->width;
+            $cropHeight = $widthScale * $height;
+            $srcX = 0;
+            $srcY = round(($this->height - $cropHeight)/2);
+        }
+
+        $thumb = imagecreatetruecolor($width, $height);
+
+        // Copy resampled makes a smooth thumbnail
+        imagecopyresampled($thumb, $this->image, 0, 0, $srcX, $srcY, $width, $height, $cropWidth, $cropHeight);
+        imagedestroy($this->image);
+
+        $this->width = $width;
+        $this->height = $height;
+        $this->image = $thumb;
+
+        if ($thumb) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Save the image to a file. Type is determined from the extension
+     *
+     * @param string $fileName
+     * @return bool
+     */
+    public function writeImage($fileName)
+    {
+        if (!$this->image || file_exists($fileName)) {
+            return false;
+        }
+
+        $ext = strtolower(substr($fileName, strrpos($fileName, '.')));
+
+        switch ($ext) {
+            case '.gif':
+                return imagegif($this->image, $fileName);
+                break;
+            case '.jpg':
+            case '.jpeg':
+                return imagejpeg($this->image, $fileName, $this->quality);
+                break;
+            case '.png':
+                return imagepng($this->image, $fileName);
+                break;
+            case '.bmp':
+                return imagewbmp($this->image, $fileName);
+                break;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Destroy image source
+     */
+    public function __destruct()
+    {
+        imagedestroy($this->image);
+    }
+}
+ 
--- application/library/Application/Image/Thumbnail.php
+++ application/library/Application/Image/Thumbnail.php
@@ -1,24 +1,7 @@
 <?php
 /**
- * Copyright (c) 2013 by Bluz PHP Team
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/skeleton
  */
 
 /**
@@ -114,16 +97,23 @@ class Thumbnail
             throw new Exception("Thumbnail image can't be save. Parent directory is not writable");
         }
 
+        // Thumbnail already exists
+        // then remove it and regenerate
+        if (file_exists($dir.'/'.$this->file)) {
+            unlink($dir.'/'.$this->file);
+        }
+
         if (class_exists('\\Imagick')) {
             $image = new \Imagick($this->path.'/'.$this->file);
-            $image->cropThumbnailImage($this->width, $this->height);
-            $image->writeimage($dir.'/'.$this->file);
         } elseif (function_exists('gd_info')) {
-            // TODO: Implementation image manipulation with gd_info
+            $image = new Gd($this->path.'/'.$this->file);
         } else {
             // return original file
             return $this->path .'/'. $this->file;
         }
+
+        $image->cropThumbnailImage($this->width, $this->height);
+        $image->writeimage($dir.'/'.$this->file);
 
         return $dir .'/'. $this->file;
     }
