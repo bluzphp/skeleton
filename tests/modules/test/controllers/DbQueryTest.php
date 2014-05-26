825eaf3c9adbf57a91e4a7fa81678c678e7258cc
AntonShevchuk <AntonShevchuk@gmail.com>
2014-05-26 12:44:56 +0300

    Added initial tests for `test/db` and `test/db-query`

---
+++ tests/modules/test/controllers/DbQueryTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/skeleton
+ */
+
+/**
+ * @namespace
+ */
+namespace Application\Tests\Test;
+
+use Application\Tests\ControllerTestCase;
+
+/**
+ * @package  Application\Tests\Test
+ * @author   Anton Shevchuk
+ * @created  26.05.14 12:37
+ */
+class DbQueryTest extends ControllerTestCase
+{
+    /**
+     * Dispatch module/controller
+     */
+    public function testControllerPage()
+    {
+        $this->dispatchRouter('/test/db-query/');
+        $this->assertOk();
+    }
+}
---
+++ tests/modules/test/controllers/DbTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Bluz PHP Team
+ * @link https://github.com/bluzphp/skeleton
+ */
+
+/**
+ * @namespace
+ */
+namespace Application\Tests\Test;
+
+use Application\Tests\ControllerTestCase;
+
+/**
+ * @package  Application\Tests\Test
+ * @author   Anton Shevchuk
+ * @created  26.05.14 12:36
+ */
+class DbTest extends ControllerTestCase
+{
+    /**
+     * Dispatch module/controller
+     */
+    public function testControllerPage()
+    {
+        $this->dispatchRouter('/test/db/');
+        $this->assertOk();
+    }
+}
