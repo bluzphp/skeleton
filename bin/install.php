cd34ca09b289de37fd6ec3c9065ebae200415368
makarova <makarova.viktoriia@gmail.com>
2014-06-13 18:41:04 +0300

    Update install.php
    Add create folders if they don't exist

--- bin/install.php
+++ bin/install.php
@@ -7,23 +7,26 @@
 // Root path, one level up
 $root = dirname(__DIR__);
 
-echo 'Set permissions' . PHP_EOL;
-if (chmod($root . '/data/cache', 0777)) {
-    echo ' ./data/cache' . PHP_EOL;
-}
-if (chmod($root . '/data/logs', 0777)) {
-    echo ' ./data/logs' . PHP_EOL;
-}
-if (chmod($root . '/data/sessions', 0777)) {
-    echo ' ./data/sessions' . PHP_EOL;
-}
-if (chmod($root . '/data/uploads', 0777)) {
-    echo ' ./data/uploads' . PHP_EOL;
-}
-if (chmod($root . '/public/uploads', 0777)) {
-    echo ' ./public/uploads' . PHP_EOL;
-}
+echo "Create folders if they don't exist and set permissions" .  PHP_EOL;
+$folders = [
+    '/data/cache',
+    '/data/logs',
+    '/data/sessions',
+    '/data/uploads',
+    '/public/uploads',
+];
 
+foreach ($folders as $folder) {
+    if (!is_dir($root . $folder)
+        && mkdir($root . $folder, 0777, true)) {
+        echo 'Created .' . $folder . PHP_EOL;
+    }
+    if (chmod($root . $folder, 0777)) {
+        echo 'Set permissions on .' . $folder . PHP_EOL;
+    }
+
+}
+unset($folder);
 echo 'Copy configuration' . PHP_EOL;
 if (copy($root . '/application/configs/app.dev.sample.php', $root . '/application/configs/app.dev.php')) {
     echo ' ./configs/app.dev.php' . PHP_EOL;
