15ddc1e55e63b36ef3eea3920ab57314d7787d26
Ravenholm <aedakftw@gmail.com>
2014-10-20 16:28:27 +0300

    Fixed method name in controller

--- application/modules/users/controllers/signout.php
+++ application/modules/users/controllers/signout.php
@@ -21,7 +21,7 @@ function () use ($view) {
      * @var Bootstrap $this
      * @var \Bluz\View\View $view
      */
-    AppAuth\Table::getInstance()->clearCookie($this->user()->id);
+    AppAuth\Table::getInstance()->removeCookieToken($this->user()->id);
     Auth::clearIdentity();
     Messages::addNotice('You are signout');
     $this->redirectTo('index', 'index');
