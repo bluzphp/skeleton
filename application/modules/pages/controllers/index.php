4111edc60d328dddbea95f7ba36ae23d4030e709
AntonShevchuk <AntonShevchuk@gmail.com>
2014-11-06 14:17:36 +0200

    Added usage of HttpCacheControl component

--- application/modules/pages/controllers/index.php
+++ application/modules/pages/controllers/index.php
@@ -8,6 +8,7 @@
 namespace Application;
 
 use Bluz\Application\Exception\NotFoundException;
+use Bluz\Proxy\HttpCacheControl;
 use Bluz\Proxy\Layout;
 use Bluz\View\View;
 
@@ -26,11 +27,21 @@ function ($alias) use ($view) {
     $page = Pages\Table::getInstance()->getByAlias($alias);
 
     if (!$page) {
+        // throw NOT FOUND exception
+        // all logic of error scenario you can found in default error controller
+        // see /application/modules/error/controllers/index.php
         throw new NotFoundException();
     } else {
+        // setup HTML layout data
         Layout::title(esc($page->title), View::POS_PREPEND);
         Layout::meta('keywords', esc($page->keywords));
         Layout::meta('description', esc($page->description));
+
+        // setup HTTP cache
+        HttpCacheControl::setPublic();
+        HttpCacheControl::setLastModified(new \DateTime($page->updated));
+
+        // assign page to view
         $view->page = $page;
     }
 };
