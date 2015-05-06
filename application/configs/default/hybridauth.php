eb565bef994b2578f62fdae7615b69714e6b93a2
yuklia <yuliakostrikova@gmail.com>
2015-05-06 17:47:49 +0300

    unused classes were removed

--- application/configs/default/hybridauth.php
+++ application/configs/default/hybridauth.php
@@ -8,7 +8,6 @@
 
 /**
  * @link http://hybridauth.sourceforge.net/userguide/Configuration.html
- * You must define provider class inside providers scope
  */
 return array(
         //"base_url" the url that point to HybridAuth Endpoint (where index.php and config.php are found)
@@ -21,7 +20,10 @@ return array(
                 "wrapper" => array( "path" => "Providers/Google.php", "class" => "Hybrid_Providers_Google" ),
                 "keys" => array("id" => "422236904670-r9jmuh7q4kc2vqpkscqtv9i1898mer4u.apps.googleusercontent.com",
                     "secret" => "3krFP90YB6F6xIADFG-Nzr1Q"),
-                "provider" => 'Application\Auth\Google'
+                "scope"           => "https://www.googleapis.com/auth/userinfo.profile ". // optional
+                    "https://www.googleapis.com/auth/userinfo.email"   , // optional
+                "access_type"     => "offline",   // optional
+                "approval_prompt" => "force",     // optional
             ),
 
             // facebook
@@ -30,15 +32,13 @@ return array(
                 "wrapper" => array( "path" => "Providers/Facebook.php", "class" => "Hybrid_Providers_Facebook" ),
                 "keys" => array("id" => "1413483462306154", "secret" => "0911925061bdb04a3d8c41129f672065"),
                 "scope"   => "email, user_about_me, user_birthday, user_hometown, publish_actions", // optional
-                "provider" => 'Application\Auth\Facebook'
             ),
 
             // twitter
             "Twitter" => array( // 'key' is your twitter application consumer key
                 "enabled" => true,
                 "wrapper" => array( "path" => "Providers/Twitter.php", "class" => "Hybrid_Providers_Twitter" ),
-                "keys" => array("key" => "eOMvWh3ODqk4A1MHEeolm0Cfv", "secret" => "aJSrRzFlJxZp60IhTPnsOetwYVZ6XBZmcd4wQbPwyc7hgIYg0M"),
-                "provider" => 'Application\Auth\Twitter'
+                "keys" => array("key" => "eOMvWh3ODqk4A1MHEeolm0Cfv", "secret" => "aJSrRzFlJxZp60IhTPnsOetwYVZ6XBZmcd4wQbPwyc7hgIYg0M")
             )
         ),
 
