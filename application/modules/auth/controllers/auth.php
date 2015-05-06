fc2566a4c18dd6599df9d3f9bd2aa24cf912139b
yuklia <yuliakostrikova@gmail.com>
2015-05-06 13:37:24 +0300

    hotfix

--- application/modules/auth/controllers/auth.php
+++ application/modules/auth/controllers/auth.php
@@ -39,22 +39,4 @@ return
                 return false;
         };
 
-
-     /*   $config = Config::getData('hybridauth');
-
-        try{
-        $hybridauth = new \Hybrid_Auth( $config );
-
-        $twitter = $hybridauth->authenticate( "Facebook" );
-
-        if ($twitter->isUserConnected()) {
-        $user_profile = $twitter->getUserProfile();
-        echo "Hi there! " . $user_profile->displayName;
-        $twitter->logout();
-        }
-        }
-        catch( Exception $e ){
-        echo "Ooophs, we got an error: " . $e->getMessage();
-        }*/
-
     };
