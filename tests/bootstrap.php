6f0b893abf45409fc9f7de6a1fd8631ad2a9d8df
yuklia <yuliakostrikova@gmail.com>
2015-05-08 11:54:15 +0300

    hotfix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,10 @@
 // Environment
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
-chdir(__DIR__);
-
 // Emulate session
 $_SESSION = array();
 $_COOKIE[session_name()] = uniqid();
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-
 $env = getenv('BLUZ_ENV') ?: 'testing';
-
 $app = \Application\Tests\BootstrapTest::getInstance();
 $app->init($env);
