c7a24c86e0c7ca95870859f0836ffce7e4968234
AntonShevchuk <AntonShevchuk@gmail.com>
2015-05-20 17:09:12 +0300

    Migrate to `password_hash` usage

--- application/configs/default/auth.php
+++ application/configs/default/auth.php
@@ -7,8 +7,11 @@
  */
 return array(
     "equals" => array(
-        "encryptFunction" => function ($password, $salt) {
-            return password_hash($password, PASSWORD_DEFAULT, ['salt' => $salt]);
+        "hash" => function ($password) {
+            return password_hash($password, PASSWORD_DEFAULT);
+        },
+        "verify" => function ($password, $hash) {
+            return password_verify($password, $hash);
         }
     )
 );
