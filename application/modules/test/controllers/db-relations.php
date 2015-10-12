60237e6160b7b63cb5c2643d9f8c690fde46da64
AntonShevchuk <AntonShevchuk@gmail.com>
2015-10-12 12:39:38 +0300

    Updated example code style

--- application/modules/test/controllers/db-relations.php
+++ application/modules/test/controllers/db-relations.php
@@ -29,31 +29,28 @@ function () use ($view) {
         ]
     );
 
-    /* @var Pages\Row */
-    $page = Pages\Table::findRow(5);
-    $user = $page->getRelation('Users');
-
     echo "<h2>Page Owner</h2>";
+    /* @var Pages\Row */
+    $page = Pages\Table::findRow(1);
+    $user = $page->getRelation('Users');
     var_dump($user);
 
-    $pages = $user->getRelations('Pages');
-
     echo "<h2>User pages</h2>";
+    $pages = $user->getRelations('Pages');
     var_dump(sizeof($pages));
 
-    $roles = $user->getRelations('Roles');
-
     echo "<h2>User roles</h2>";
+    $roles = $user->getRelations('Roles');
     var_dump($roles);
 
     echo "<h2>User with all relations</h2>";
     var_dump($user);
 
+
+    echo "<h2>User - Page relation</h2>";
     $result = Db::fetchRelations(
         "SELECT '__users', u.*, '__pages', p.* FROM users u LEFT JOIN pages p ON p.userId = u.id"
     );
-
-    echo "<h2>User - Page relation</h2>";
     var_dump($result);
 
     return false;
