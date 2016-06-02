2e77f0bdfc5d84415acde4ac3a76cd563582927f
AntonShevchuk <AntonShevchuk@gmail.com>
2016-06-02 15:19:07 +0300

    Fixed Categories Crud

--- application/models/Categories/Crud.php
+++ application/models/Categories/Crud.php
@@ -11,52 +11,9 @@ namespace Application\Categories;
 
 /**
  * Class Crud
- * @package Application\Categories
  *
- * @method Table getTable()
+ * @package Application\Categories
  */
 class Crud extends \Bluz\Crud\Table
 {
-    /**
-     * @var array
-     */
-    private $arrayWithTree = [];
-
-    /**
-     * @param mixed $data
-     * @return int|void
-     */
-    public function deleteOne($data)
-    {
-        $table = Table::getInstance();
-        $tree = $table->buildTree($data['id']);
-
-        if (!isset($tree[0]['children'])) {
-            parent::deleteOne($data);
-        } else {
-            $allSubCategories = $this->treeToArray($tree);
-
-            foreach ($allSubCategories as $categoryId) {
-                parent::deleteOne(['id' => $categoryId]);
-            }
-        }
-    }
-
-    /**
-     * @param array $tree
-     * @return array
-     */
-    private function treeToArray($tree)
-    {
-        foreach ($tree as $node) {
-            if (empty($node['children'])) {
-                $this->arrayWithTree[] = $node['id'];
-            } else {
-                $this->arrayWithTree[] = $node['id'];
-                $this->treeToArray($node['children']);
-            }
-        }
-
-        return $this->arrayWithTree;
-    }
 }
