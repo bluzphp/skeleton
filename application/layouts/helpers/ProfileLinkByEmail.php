3231f169c196c81b0ceecc02704159cc53173dee
Anton <AntonShevchuk@gmail.com>
2016-12-16 18:37:43 +0200

    Updated helper `ProfileLinkByEmail`

--- application/layouts/helpers/ProfileLinkByEmail.php
+++ application/layouts/helpers/ProfileLinkByEmail.php
@@ -10,24 +10,24 @@
 namespace Application\Layout\Helper;
 
 use Application\Users;
-use Bluz\Proxy\Layout;
 
 return
     /**
-     * get profile link by email user
+     * Get profile link by email user
      *
      * @param string $email
-     *
+     * @param array $attributes
      * @return string
      */
-    function ($email) {
+    function ($email, array $attributes = []) {
         /**
          * @var \Application\Users\Row $user
          */
         $row = Users\Table::findRow(['email' => $email]);
 
         if (!empty($row)) {
-            return Layout::ahref($row->login, ['users', 'profile'], ['id' => $row->id]);
+            $attributes['data-id'] = $row->id;
+            return $this->ahref($row->login, ['users', 'profile'], $attributes);
         }
-        return false;
+        return '';
     };
