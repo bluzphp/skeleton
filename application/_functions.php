53038b069f972c1992f8c963a168c054e446d7ca
Anton Shevchuk <AntonShevchuk@gmail.com>
2019-02-18 18:51:37 +0200

    Added `@` support for `errorHandler`

--- application/_functions.php
+++ application/_functions.php
@@ -14,10 +14,15 @@ namespace Application;
  * @param string  $file
  * @param integer $line
  *
+ * @return bool
  * @throws \ErrorException
  */
 function errorHandler($severity, $message, $file = null, $line = null)
 {
+    // Handles @ error suppression
+    if (error_reporting() === 0) {
+        return false;
+    }
     throw new \ErrorException($message, 0, $severity, $file, $line);
 }
 
